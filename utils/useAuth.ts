ec8116e0784d90fda087fe7419ab76e4ec3c5bd0
Selva-Torus <157101986+Selva-Torus@users.noreply.github.com>
2024-04-23 16:23:44 +0530

    bhjvbdhjsvd

---
+++ utils/useAuth.ts
@@ -0,0 +1,36 @@
+"use client";
+import Keycloak from "keycloak-js";
+import React, { useEffect, useRef, useState } from "react";
+ 
+const useAuth = () => {
+  const useRefs = useRef(false);
+  const [token, settoken] = useState<any>("");
+ 
+  const url = process.env.NEXT_PUBLIC_SECRET_KEY_KEYCLOAK_URL;
+  const realm: string | any = process.env.NEXT_PUBLIC_SECRET_KEY_REALM;
+  const clientId: string | any = process.env.NEXT_PUBLIC_SECRET_KEY_CLIENT;
+  const [keyCloak, setkeycloak] = useState<any>(null);
+ 
+  useEffect(() => {
+    if (useRefs.current) return;
+    useRefs.current = true;
+    const client = new Keycloak({
+      url: url,
+      realm: realm,
+      clientId: clientId,
+    });
+    setkeycloak(client);
+    client
+      .init({
+        onLoad: "login-required",
+        redirectUri: "http://localhost:3000",
+      })
+      .then(() => {
+        settoken(client.token);
+      });
+  }, []);
+ 
+  return [keyCloak, token];
+};
+ 
+export default useAuth;
